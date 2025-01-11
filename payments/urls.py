cc4f38fdbb223563849686b671c270873f34ae68
exaon18 <exaontech@gamil.com>
2025-01-10 23:30:54 -0800

    updates

--- payments/urls.py
+++ payments/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.payment, name='payment'),
+    
     path('initiate/', views.initiate_payment, name='initiate_payment'), 
     path('notification/', views.payment_notification, name='payment_notification'),
     
--- telebir/settings.py
+++ telebir/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'psyments',
+    'payments',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
